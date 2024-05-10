693cea6a4eb9824e2af83bd7762cf8496cf1b14d
Muhammet Kılıçarslan Karapınar <mkilicarslankarapinar@gmail.com>
2024-05-10 16:53:12 +0300

    Custom Identity Validator projeye eklendi ve default validasyon mesajları düzenlendi.

--- TraversalCoreProject/Program.cs
+++ TraversalCoreProject/Program.cs
@@ -1,46 +1,44 @@
 using DataAccessLayer.Concrete;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Authorization;
+using TraversalCoreProject.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddDbContext<Context>();
-builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>();
+builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>().
+    AddErrorDescriber<CustomIdentityValidator>().
+    AddTokenProvider<DataProtectorTokenProvider<AppUser>>(TokenOptions.DefaultProvider).
+    AddEntityFrameworkStores<Context>();// Tek sefer çaðýrýldý.
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddMvc(config =>
+builder.Services.AddControllersWithViews(config =>
 {
-	var policy = new AuthorizationPolicyBuilder()
-	.RequireAuthenticatedUser()
-	.Build();
-	config.Filters.Add(new AuthorizeFilter(policy));
+    var policy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+    config.Filters.Add(new AuthorizeFilter(policy));
 });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAuthentication();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "areas",
+    pattern: "{controller=Default}/{action=Index}/{id?}");
 
 app.Run();
